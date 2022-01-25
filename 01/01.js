6f0841cd935cdd64be5a9803068b4703bf4ba1a9
Pavel <76093403+pavelkrasulin@users.noreply.github.com>
2022-01-25 13:56:36 +0400

    Изменяет структуру проекта и исправляет ДЗ-01

---
+++ 01/01.js
@@ -0,0 +1,42 @@
+let defaultUser = {name: 'My name is...', surname:'My surname is...', age: 18};
+
+function userToConsole() {
+    let userName = prompt('Hello, dear User! Please answer, what is your name?', defaultUser.name);
+
+    while (userName === '' || userName === defaultUser.name) {
+        alert('Please, enter your name');
+        userName = prompt('Hello, dear User! Please answer, what is your name?', defaultUser.name);
+    }
+
+    if (userName === null) {
+        return;
+    }
+
+    let userSurname = prompt(`OK, ${userName}, what is your surname?`, defaultUser.surname);
+
+    while (userSurname === '' || userSurname === defaultUser.surname) {
+        alert('Please, enter your surname');
+        userSurname = prompt(`OK, ${userName}, what is your surname?`, defaultUser.surname);
+    }
+        
+    if (userSurname === null) {
+        return;
+    }
+
+    let userAge = prompt(`Very well, ${userName} ${userSurname}, what is your age?`, defaultUser.age);
+
+    while (userAge === '' || userAge <= 0) {
+        alert('Please, enter your age');
+        userAge = prompt(`Very well, ${userName} ${userSurname}, what is your age?`, defaultUser.age);
+    }
+
+    if (userAge === null) {
+        return;
+    }
+
+    let user = {userName, userSurname, userAge};
+
+    console.log(user);
+}
+
+userToConsole();
