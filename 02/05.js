989b43b71d07ea856765d9cec33b498a177417bb
Pavel <76093403+pavelkrasulin@users.noreply.github.com>
2022-04-27 22:37:39 +0400

    Добавляет правки по ДЗ 02.5

--- 02/05.js
+++ 02/05.js
@@ -1,7 +1,15 @@
 function guessNum(num) {
     let rand = Math.floor(1 + Math.random() * 10);
 
-    while (num != rand) {
+    while (isNumber(num) === false) {
+        if (isNull(num) === true) {
+            return;
+        }
+        alert(`Вы ввели "${num}"! Пожалуйста введите корректное число!`);
+        num = prompt('Угадайте число от 1 до 10');
+    }
+
+    while (Number(num) !== rand) {
         if (isNull(num) === true) {
             return;
         }
@@ -12,6 +20,11 @@ function guessNum(num) {
     return alert(`Вы угадали, это число - ${rand}!`);
 }
 
-const userInput = getUserInput('Угадайте число от 1 до 10');
+// Странно что вроде бы переменная userInput должна принимать функцию getUserInput(),
+// в которой уже есть проверка на isNumber, но она не срабатывает
+
+// const userInput = getUserInput('Угадайте число от 1 до 10');
+
+const userInput = prompt('Угадайте число от 1 до 10');
 
 guessNum(userInput);
