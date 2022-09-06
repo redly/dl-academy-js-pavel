72a59b3356f42e18ae5fac1d8d5528b10116852e
Pavel <76093403+pavelkrasulin@users.noreply.github.com>
2022-09-06 20:11:56 +0400

    Добавляет правки по main-page, profile-page и js

--- source/js/script.js
+++ source/js/script.js
@@ -29,6 +29,12 @@ if (mainSlider) {
     const mainSwiper = new Swiper('.swiper-main-slider', {
         initialSlide: Number(localStorage.getItem('activeSlide')) || 0,
 
+        on: {
+            activeIndexChange: function () {
+                localStorage.setItem('activeSlide', this.realIndex);
+            },
+        },
+
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
@@ -37,10 +43,6 @@ if (mainSlider) {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-    });
-
-    mainSwiper?.on('activeIndexChange', function () {
-        localStorage.setItem('activeSlide', mainSwiper.realIndex);
     });
 }
 
@@ -105,25 +107,6 @@ forms?.forEach((form) => {
 
     const inputs = form.querySelectorAll('.js-validFormField');
 
-    const formMsgClassNames = {
-        msg: 'contacts-form__message',
-        errorMsg: 'contacts-form__message--error',
-        rightMsg: 'contacts-form__message--right',
-    };
-
-    const formMsg = formMsgClassNames.msg;
-    const error = formMsgClassNames.errorMsg;
-    const right = formMsgClassNames.rightMsg;
-
-    function addElement(className, text) {
-        const element = document.createElement('span');
-
-        element.className = `${formMsg} ${className}`;
-        element.innerText = text;
-
-        return element;
-    }
-
     function delElement() {
         const removeElement = form.querySelectorAll('.contacts-form__message, .contacts-form__message--error, .contacts-form__message--right');
 
@@ -142,31 +125,52 @@ forms?.forEach((form) => {
         delElement();
 
         inputs?.forEach((input) => {
-            function addClassToInput(className) {
-                input.classList.add(className);
-            }
+            const formMsgClassNames = {
+                msg: 'contacts-form__message',
+                errorMsg: 'contacts-form__message--error',
+                rightMsg: 'contacts-form__message--right',
+            };
+
+            const formMsg = formMsgClassNames.msg;
+            const error = formMsgClassNames.errorMsg;
+            const right = formMsgClassNames.rightMsg;
+
+            const fieldClassNames = {
+                error: 'contacts-form__field--error',
+                right: 'contacts-form__field--right',
+            };
+
+            const errorField = fieldClassNames.error;
+            const rightField = fieldClassNames.right;
 
             function validateField(className, text) {
-                const element = addElement(className, text);
+                // Создание подписи под полем
+                const element = document.createElement('span');
+
+                element.className = `${formMsg} ${className}`;
+                element.innerText = text;
 
                 input.parentElement?.insertAdjacentElement('beforeend', element);
 
+                // Окрашивание рамки поля в зависимости от подписи
                 if (className === error) {
-                    addClassToInput('custom-form-field--error');
+                    input.classList.add(errorField);
                 } else  if (className === right) {
-                    addClassToInput('custom-form-field--right');
+                    input.classList.add(rightField);
                 }
             }
 
             function validateInput(regExp, text) {
-                if (regExp === false) {
+                if (!input.value) {
+                    validateField(error, messages.empty);
+                } else if (regExp === false) {
                     validateField(error, text);
                 } else {
                     validateField(right, messages.right);
                 }
             }
 
-            function isNumber(num) {
+            function isPositiveNumber(num) {
                 if (isNaN(num) || num === '' || num <= 0) {
                     return false;
                 }
@@ -177,70 +181,72 @@ forms?.forEach((form) => {
             const messages = {
                 right: 'All right',
                 empty: 'This field is required',
-                email: 'Please enter a valid email address (your entry is not in the format "somebody@example.com")',
+                email: 'Please enter a valid email address (your entry is not in the format "somebody@example.com" or "somebody@local")',
                 password: {
                     isFalse: 'Your password must contain letters and numbers',
                     repeat: 'Passwords do not match. Please check the identity of the passwords in both fields',
                 },
-                name: 'Your name must contain only letters',
                 phone: 'Please enter a valid phone number (your entry is not in the format "+7 977 777 77 77")',
-                location: 'Please enter a valid location address (your entry is not in the format "Saint-Petersburg" or "Moscow")',
                 age: 'Please enter a correct age',
-                subject: 'Your subject should not contain more than 50 characters',
-                message: 'Your message should not contain more than 300 characters',
             };
 
             const regExp = {
-                email: /^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i,
+                email: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
                 phone: /^(\s*)?(\+)?([-_():=+]?\d[- _():=+]?){10,14}(\s*)?$/,
                 password: /^[0-9a-z]+/i,
-                name: /^([a-zа-яё]+[\s]{0,1}[a-zа-яё]+[\s]{0,1}[a-zа-яё]+)$/i,
-                location: /^[a-zа-яё\-]+/i,
-                subject: /^(?=[a-zа-яё0-9])[a-zа-яё0-9\s]{0,49}[a-zа-яё0-9]$/i,
-                message: /^(?=[a-zа-яё0-9])[a-zа-яё0-9\s]{0,299}[a-zа-яё0-9]$/i,
             };
 
             const regExpTest = {
                 validEmail: regExp.email.test(input.value),
                 validPhone: regExp.phone.test(input.value),
                 validPassword: regExp.password.test(input.value),
-                validName: regExp.name.test(input.value),
-                validLocation: regExp.location.test(input.value),
-                validSubject: regExp.subject.test(input.value),
-                validMessage: regExp.message.test(input.value),
             };
 
-            if (!input.value) {
-                validateField(error, messages.empty);
-            } else if (input.name === 'email') {
-                validateInput(regExpTest.validEmail, messages.email);
-            } else if (input.name === 'password') {
-                validateInput(regExpTest.validPassword, messages.password.isFalse);
-            } else if (input.name === 'password-repeat') {
-                const password = form.querySelector('.password');
-                const passwordRepeat = form.querySelector('.password-repeat');
+            /*
+                Попробовал поместить проверки в data атрибуты,
+                но метод test() не работает с input.dataset.pattern например,
+                т.к. он возвращает строку, а не объект.
+            */
 
-                if (passwordRepeat?.value !== password?.value && passwordRepeat?.value !== '') {
-                    validateField(error, messages.password.repeat);
-                } else if (passwordRepeat?.value === password?.value && passwordRepeat?.value !== '') {
-                    validateField(right, messages.right);
-                }
-            } else if (input.name === 'name') {
-                validateInput(regExpTest.validName, messages.name);
-            } else if (input.name === 'location') {
-                validateInput(regExpTest.validLocation, messages.location);
-            } else if (input.name === 'age') {
-                if (isNumber(input.value) === false) {
-                    validateField(error, messages.age);
-                } else {
-                    validateField(right, messages.right);
-                }
-            } else if (input.name === 'phone') {
-                validateInput(regExpTest.validPhone, messages.phone);
-            } else if (input.name === 'subject') {
-                validateInput(regExpTest.validSubject, messages.subject);
-            } else if (input.name === 'message') {
-                validateInput(regExpTest.validMessage, messages.message);
+            switch (input.type) {
+                case 'email':
+                    validateInput(regExpTest.validEmail, messages.email);
+                    break;
+                case 'password':
+                    if (input.name === 'password') {
+                        validateInput(regExpTest.validPassword, messages.password.isFalse);
+                    } else if (input.name === 'password-repeat') {
+                        const password = form.querySelector('.password');
+                        const passwordRepeat = form.querySelector('.password-repeat');
+
+                        if (!input.value) {
+                            validateField(error, messages.empty);
+                        } else if (passwordRepeat?.value !== password?.value && passwordRepeat?.value !== '') {
+                            validateField(error, messages.password.repeat);
+                        } else if (passwordRepeat?.value === password?.value && passwordRepeat?.value !== '') {
+                            validateField(right, messages.right);
+                        }
+                    }
+
+                    break;
+                case 'tel':
+                    validateInput(regExpTest.validPhone, messages.phone);
+                    break;
+                case 'number':
+                    if (input.name === 'age') {
+                        if (!input.value) {
+                            validateField(error, messages.empty);
+                        } else if (isPositiveNumber(input.value) === false) {
+                            validateField(error, messages.age);
+                        } else {
+                            validateField(right, messages.right);
+                        }
+                    }
+
+                    break;
+                default:
+                    validateInput();
+                    break;
             }
         });
     });
